05f1dca3388ddabea73942a55bb971346191157b
juxuny <juxuny@gmail.com>
2021-04-13 12:32:48 +0800

    create container

--- docker.go
+++ docker.go
@@ -1,8 +1,25 @@
 package supervisor
 
 import (
+	"context"
+	"fmt"
+	"github.com/docker/docker/api/types"
+	"github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/client"
+	"github.com/pkg/errors"
+	"io"
 	"os"
+	"strings"
+	"time"
+)
+
+const (
+	DefaultTimeout = time.Second * 15
+)
+
+var (
+	NotFound        = errors.New("not found")
+	containerPrefix = "sup"
 )
 
 type DockerClientConfig struct {
@@ -42,4 +59,126 @@ func NewDockerClient(config DockerClientConfig) (*DockerClient, error) {
 		return nil
 	})
 	return sdk, err
+}
+
+func (t *DockerClient) findImage(ctx context.Context, imageWithTag string) (ret types.ImageSummary, err error) {
+	list, err := t.ImageList(ctx, types.ImageListOptions{})
+	if err != nil {
+		return ret, errors.Wrap(err, "get image list failed")
+	}
+	for _, item := range list {
+		for _, repoTag := range item.RepoTags {
+			if repoTag == imageWithTag {
+				return item, nil
+			}
+		}
+	}
+	return ret, NotFound
+}
+
+func (t *DockerClient) Apply(ctx context.Context, deployConfig DeployConfig) (id string, err error) {
+	imageWithTag := deployConfig.Image + ":" + deployConfig.Tag
+	if deployConfig.PullRetryTimes <= 0 {
+		deployConfig.PullRetryTimes = 3
+	}
+	for i := 0; i < int(deployConfig.PullRetryTimes); i++ {
+		reader, err := t.ImagePull(ctx, imageWithTag, types.ImagePullOptions{})
+		if err != nil {
+			panic(err)
+		}
+		_, _ = io.Copy(os.Stdout, reader)
+		if _, err := t.findImage(ctx, imageWithTag); err != nil {
+			if err != NotFound {
+				return id, err
+			}
+		} else {
+			break
+		}
+		fmt.Println("retry:", i+1)
+	}
+
+	containerName := strings.Join([]string{containerPrefix, deployConfig.Name}, "-")
+	resp, err := t.ContainerCreate(ctx, &container.Config{
+		Image: imageWithTag,
+	}, &container.HostConfig{
+		AutoRemove: true,
+	}, nil, nil, containerName)
+	if err != nil {
+		return "", errors.Wrap(err, "create container failed")
+	}
+	if err := t.ContainerStart(ctx, resp.ID, types.ContainerStartOptions{}); err != nil {
+		return "", errors.Wrap(err, "start container failed")
+	}
+	//config := container.Config{
+	//	Hostname:        strings.Join([]string{containerPrefix, deployConfig.Name}, "-"),
+	//	Domainname:      strings.Join([]string{containerPrefix, deployConfig.Name}, "-"),
+	//	User:            "root",
+	//	AttachStdin:     false,
+	//	AttachStdout:    false,
+	//	AttachStderr:    false,
+	//	ExposedPorts:    nil,
+	//	Tty:             false,
+	//	OpenStdin:       false,
+	//	StdinOnce:       false,
+	//	Env:             nil,
+	//	Cmd:             nil,
+	//	Healthcheck:     nil,
+	//	ArgsEscaped:     false,
+	//	Image:           deployConfig.Image + ":" + deployConfig.Tag,
+	//	Volumes:         nil,
+	//	WorkingDir:      "",
+	//	Entrypoint:      nil,
+	//	NetworkDisabled: false,
+	//	MacAddress:      "",
+	//	OnBuild:         nil,
+	//	Labels:          nil,
+	//	StopSignal:      "",
+	//	StopTimeout:     nil,
+	//	Shell:           nil,
+	//}
+	//hostConfig := container.HostConfig{
+	//	Binds:           nil,
+	//	ContainerIDFile: "",
+	//	LogConfig:       container.LogConfig{},
+	//	NetworkMode:     "",
+	//	PortBindings:    nil,
+	//	RestartPolicy:   container.RestartPolicy{},
+	//	AutoRemove:      false,
+	//	VolumeDriver:    "",
+	//	VolumesFrom:     nil,
+	//	CapAdd:          nil,
+	//	CapDrop:         nil,
+	//	CgroupnsMode:    "",
+	//	DNS:             nil,
+	//	DNSOptions:      nil,
+	//	DNSSearch:       nil,
+	//	ExtraHosts:      nil,
+	//	GroupAdd:        nil,
+	//	IpcMode:         "",
+	//	Cgroup:          "",
+	//	Links:           nil,
+	//	OomScoreAdj:     0,
+	//	PidMode:         "",
+	//	Privileged:      false,
+	//	PublishAllPorts: false,
+	//	ReadonlyRootfs:  false,
+	//	SecurityOpt:     nil,
+	//	StorageOpt:      nil,
+	//	Tmpfs:           nil,
+	//	UTSMode:         "",
+	//	UsernsMode:      "",
+	//	ShmSize:         0,
+	//	Sysctls:         nil,
+	//	Runtime:         "",
+	//	ConsoleSize:     [2]uint{},
+	//	Isolation:       "",
+	//	Resources:       container.Resources{},
+	//	Mounts:          nil,
+	//	MaskedPaths:     nil,
+	//	ReadonlyPaths:   nil,
+	//	Init:            nil,
+	//}
+	//nc := network.NetworkingConfig{}
+	//t.ContainerCreate(ctx, &config, &hostConfig, &nc, nil, strings.Join([]string{containerPrefix, deployConfig.Name}, "-"))
+	return
 }
--- docker_test.go
+++ docker_test.go
@@ -22,3 +22,24 @@ func TestDockerClient_ContainerList(t *testing.T) {
 		t.Log(item.ID, item.Status, item.Names)
 	}
 }
+
+func TestDockerClient_Apply(t *testing.T) {
+	c, err := NewDockerClient(NewDefaultDockerConfig())
+	if err != nil {
+		t.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), DefaultTimeout)
+	defer cancel()
+	_, err = c.Apply(ctx, DeployConfig{
+		ServicePort: 8080,
+		Name:        "web",
+		Image:       "juxuny/go-web",
+		Tag:         "latest",
+		Mounts:      nil,
+		EnvData:     "",
+		Envs:        nil,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+}
