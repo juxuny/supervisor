780513c85da0a52c72258a27757b6a3392008ec4
juxuny <juxuny@gmail.com>
2021-05-28 14:37:40 +0800

    proxy auto restart

--- docker.go
+++ docker.go
@@ -189,6 +189,7 @@ func (t *DockerClient) initProxy(ctx context.Context, deployConfig DeployConfig,
 		},
 		Env: t.createProxyEnv(deployConfig),
 	}, &container.HostConfig{
+		AutoRemove:    deployConfig.Restart == "" || deployConfig.Restart == "no",
 		RestartPolicy: container.RestartPolicy{Name: deployConfig.Restart},
 		PortBindings: nat.PortMap{
 			nat.Port(fmt.Sprintf("%d", deployConfig.ProxyPort)): []nat.PortBinding{
